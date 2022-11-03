59f06e117b9b0fe7a0dc11fb40c98cf902b5cb57
Flashbrv <belnitskiy@i.ua>
2022-11-03 16:02:57 +0200

    Init commit

---
+++ src/main/java/com/example/fishingstuffshopbackend/FishingStuffShopBackendApplication.java
@@ -0,0 +1,13 @@
+package com.example.fishingstuffshopbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FishingStuffShopBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FishingStuffShopBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/fishingstuffshopbackend/FishingStuffShopBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.fishingstuffshopbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FishingStuffShopBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
